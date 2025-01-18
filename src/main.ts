748cd0bb2bb9ba8a5bd79ecb2a96880c726bda7a
antonina.m <antonina.m@itfox-web.com>
2025-01-18 19:38:48 +0300

    fix: style

--- src/main.ts
+++ src/main.ts
@@ -8,30 +8,28 @@ import Input from "./components/Input";
 
 Handlebars.registerPartial("Input", Input);
 document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("app");
+
   const path = window.location.pathname;
-  if (path.includes("auth.html")) {
-    const root = document.getElementById("app");
+  if (path.includes("auth")) {
     const nFn = Handlebars.compile(Auth);
     if (root) {
       root.innerHTML = nFn({});
     }
   }
-  if (path.includes("register.html")) {
-    const root = document.getElementById("app");
+  if (path.includes("register")) {
     const nFn = Handlebars.compile(Register);
     if (root) {
       root.innerHTML = nFn({});
     }
   }
-  if (path.includes("not-found.html")) {
-    const root = document.getElementById("app");
+  if (path.includes("not-found")) {
     const nFn = Handlebars.compile(NotFound);
     if (root) {
       root.innerHTML = nFn({});
     }
   }
-  if (path.includes("error.html")) {
-    const root = document.getElementById("app");
+  if (path.includes("error")) {
     const nFn = Handlebars.compile(ErrorPage);
     if (root) {
       root.innerHTML = nFn({});
