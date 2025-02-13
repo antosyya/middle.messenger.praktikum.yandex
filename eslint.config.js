7c2f6ec1d9a9678ed4c99a53075e4cf9372c1ecb
antonina.m <antonina.m@itfox-web.com>
2025-02-13 19:29:08 +0300

    fix: tslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,30 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
+// import globals from "globals";
+// import tseslint from "typescript-eslint";
 
-/** @type {import('eslint').Linter.Config[]} */
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   { ignores: ["node_modules/", "dist/", "build/", "**/*.min.js"] },
+//   { files: ["**/*.{js,mjs,cjs,ts}"] },
+//   { languageOptions: { globals: globals.browser } },
+//   ...tseslint.configs.recommended,
+// ];
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+
 export default [
+  js.configs.recommended,
   { ignores: ["node_modules/", "dist/", "build/", "**/*.min.js"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.browser } },
-  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/ban-ts-comment": "warn",
+    },
+  },
 ];
