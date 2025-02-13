46d614b74985dfd48f47352e6871045b0fca57af
antonina.m <antonina.m@itfox-web.com>
2025-02-13 19:31:40 +0300

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,3 @@
-// import globals from "globals";
-// import tseslint from "typescript-eslint";
-
-// /** @type {import('eslint').Linter.Config[]} */
-// export default [
-//   { ignores: ["node_modules/", "dist/", "build/", "**/*.min.js"] },
-//   { files: ["**/*.{js,mjs,cjs,ts}"] },
-//   { languageOptions: { globals: globals.browser } },
-//   ...tseslint.configs.recommended,
-// ];
 import js from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 
