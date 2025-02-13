4adf6516afb09f3e78d1c7cfe3b7e633a52c47e7
antonina.m <antonina.m@itfox-web.com>
2025-02-13 19:16:38 +0300

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { ignores: ["node_modules/", "dist/", "build/", "**/*.min.js"] },
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   ...tseslint.configs.recommended,
